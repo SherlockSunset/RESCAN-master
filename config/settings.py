53f32b443f7e8d23d90c2a4dc98d540a349b8a73
SherlockSunset <2413502484@qq.com>
2018-09-05 09:34:52 +0800

    提交文件

---
+++ config/settings.py
@@ -0,0 +1,43 @@
+import os
+import logging
+
+channel = 24
+stage_num = 4
+depth = 7 # >=3
+use_se = True 
+uint = 'RNN' # ['Conv', 'RNN', 'GRU', 'LSTM']
+frame = 'Add' # ['Add', 'Full']
+
+aug_data = False # Set as False for fair comparison
+
+batch_size = 30
+#patch_size = 64
+patch_size = 8
+lr = 5e-3
+
+data_dir = '../dataset'
+log_dir = '../logdir'
+show_dir = '../showdir'
+model_dir = '../models'
+
+log_level = 'info'
+model_path = os.path.join(model_dir, 'latest')
+save_steps = 400
+
+#num_workers = 0
+num_workers = 1
+num_GPU = 1
+#device_id = 3
+device_id = 0
+
+logger = logging.getLogger('train')
+logger.setLevel(logging.INFO)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+
